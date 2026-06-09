9ea584cce11beaf27cfc5766c7ed273114a985f5
Zenekho <h.bkr@outlook.fr>
2026-06-09 14:49:27 +0200

    remove browser folder for github pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,5 +10,5 @@ import { Footer } from "./public/footer/footer";
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('home');
+  protected readonly title = signal('connect');
 }

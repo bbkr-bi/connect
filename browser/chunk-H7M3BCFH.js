020f5fa8983fb32a25394822b0c0a95e818ca0d0
Zenekho <h.bkr@outlook.fr>
2026-06-09 13:42:41 +0200

    Auto-generated commit

---
+++ browser/chunk-H7M3BCFH.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-NUVE6LF6.js").then(o=>o.Thanks)}];export{t as default};
---
+++ browser/chunk-L3HIYNTW.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-4KCKNE6E.js").then(o=>o.Home)}];export{t as default};

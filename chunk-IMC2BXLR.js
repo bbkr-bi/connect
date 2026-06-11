5ea9c1a17c757bd1e4dedbd288b275a49c1e03c9
Zenekho <h.bkr@outlook.fr>
2026-06-11 12:20:24 +0200

    Auto-generated commit

---
+++ chunk-IMC2BXLR.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-W7EP5SC4.js").then(o=>o.Thanks)}];export{t as default};
--- chunk-K7ERJV6C.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-2NFLSA4Y.js";var o=[{path:"",loadComponent:()=>import("./chunk-NCBDABQ3.js").then(t=>t.Contact)}];export{o as default};
